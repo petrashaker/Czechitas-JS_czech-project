ae53ab80a88177b41aae840377a70966d87594a0
Jakub Žák <mail@jakubzak.eu>
2021-05-09 22:18:32 +0200

    Reformátoval jsem kód.

--- renderer-process/components/carousel/carousel.js
+++ renderer-process/components/carousel/carousel.js
@@ -1,41 +1,42 @@
 export class Carousel {
-    constructor() {
-        this.carouselItemStart = 0;
+  constructor() {
+    this.carouselItemStart = 0;
 
-        this.buttonLeft = document.querySelector('#carousel-button-left');
-        this.buttonRight = document.querySelector('#carousel-button-right');
+    this.buttonLeft = document.querySelector('#carousel-button-left');
+    this.buttonRight = document.querySelector('#carousel-button-right');
 
-        this.checkButtonsVisibilty = function checkButtonsVisibilty(){};
+    this.checkButtonsVisibilty = function checkButtonsVisibilty() {};
 
-        this.buttonLeft.addEventListener('click', () => {
-            this.carouselItemStart --;
-            this.populateNewsCarousel();
-        });
+    this.buttonLeft.addEventListener('click', () => {
+      this.carouselItemStart--;
+      this.populateNewsCarousel();
+    });
 
-        this.buttonRight.addEventListener('click', () => {
-            this.carouselItemStart ++;
-            this.populateNewsCarousel();
-        });
+    this.buttonRight.addEventListener('click', () => {
+      this.carouselItemStart++;
+      this.populateNewsCarousel();
+    });
 
+  }
+
+  populateNewsCarousel(news) {
+    if (undefined !== news) {
+      this.articles = news;
     }
-
-    populateNewsCarousel(news) {
-        if (undefined !== news) {
-            this.articles = news;
-        }
-        this.header.innerText = ''; //pokud bychom nedali, zprávy při posouvání by se dvojily, tímto se vyčistí div a znovu se tam vytáhnou další zprávy a tak pořád dokola
-        for(let i = this.carouselItemStart; i < (this.carouselItemStart + this.carouselItemCount); i ++) { //startAt přidáno pro posouvání carouselu
-            const newsValue = news[i];
-            const newsArticle = new NewsArticle();
-            const newsDiv = newsArticle.createDivForNews(newsValue);
-            this.header.appendChild(newsDiv); //append = připojit na konec, tzn. nahoře se header vyčistí (prázdný string) a zase se přidá další zpráva
-        }
-        this.checkButtonsVisibilty();
+    this.header.innerText = ''; //pokud bychom nedali, zprávy při posouvání by se dvojily, tímto se vyčistí div a znovu se tam vytáhnou další zprávy a tak pořád dokola
+    for (let i = this.carouselItemStart; i <
+    (this.carouselItemStart + this.carouselItemCount); i++) { //startAt přidáno pro posouvání carouselu
+      const newsValue = news[i];
+      const newsArticle = new NewsArticle();
+      const newsDiv = newsArticle.createDivForNews(newsValue);
+      this.header.appendChild(newsDiv); //append = připojit na konec, tzn. nahoře se header vyčistí (prázdný string) a zase se přidá další zpráva
     }
+    this.checkButtonsVisibilty();
+  }
 }
 
-
-function checkButtonsVisibilty(){
-    buttonLeft.hidden = carouselItemStart === 0;
-    buttonRight.hidden = carouselItemStart >= (articles.length - carouselItemCount);
+function checkButtonsVisibilty() {
+  buttonLeft.hidden = carouselItemStart === 0;
+  buttonRight.hidden = carouselItemStart >=
+      (articles.length - carouselItemCount);
 }
