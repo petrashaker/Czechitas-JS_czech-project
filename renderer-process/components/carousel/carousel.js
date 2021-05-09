b64dd3865227790dff4e7faefef56d5b7e864d3f
Petra Shaker <petra.shaker@gmail.com>
2021-05-09 21:46:13 +0200

    ukol-c-3 cast 1

---
+++ renderer-process/components/carousel/carousel.js
@@ -0,0 +1,38 @@
+export class Carousel {
+    constructor() {
+        this.carouselItemStart = 0;
+
+        this.buttonLeft = document.querySelector('#carousel-button-left');
+        this.buttonRight = document.querySelector('#carousel-button-right');
+
+        this.checkButtonsVisibilty = function checkButtonsVisibilty(){};
+    }
+
+    populateNewsCarousel(news) { 
+        this.articles = news;
+        this.header.innerText = ''; //pokud bychom nedali, zprávy při posouvání by se dvojily, tímto se vyčistí div a znovu se tam vytáhnou další zprávy a tak pořád dokola
+        for(let i = this.carouselItemStart; i < (this.carouselItemStart + this.carouselItemCount); i ++) { //startAt přidáno pro posouvání carouselu
+            const newsValue = news[i];
+            const newsArticle = new NewsArticle();
+            const newsDiv = newsArticle.createDivForNews(newsValue);
+            this.header.appendChild(newsDiv); //append = připojit na konec, tzn. nahoře se header vyčistí (prázdný string) a zase se přidá další zpráva
+        }
+        this.checkButtonsVisibilty();
+    }
+}
+
+
+buttonLeft.addEventListener('click', () => {
+    this.carouselItemStart --;
+    populateNewsCarousel(articles, carouselItemStart);
+});
+
+buttonRight.addEventListener('click', () => {
+    this.carouselItemStart ++;
+    populateNewsCarousel(articles, carouselItemStart);
+});
+
+function checkButtonsVisibilty(){
+    buttonLeft.hidden = carouselItemStart === 0;
+    buttonRight.hidden = carouselItemStart >= (articles.length - carouselItemCount);
+}
--- renderer-process/main.js
+++ renderer-process/main.js
@@ -1,97 +1,26 @@
 import { NewsArticle } from './components/news-article/news-article.js';
+import { Carousel } from './components/carousel/carousel.js'
 
-// INFO Z 1. LEKCE
-/* 
-const header = document.querySelector('header.header-news');
-const someDiv = document.createElement('div');
-header.appendChild(someDiv); // vytvoří nový div v headeru 
-someDiv.classList.add('ad-banner'); // přidá třídu k prvku div 
-const myAge = 87;
-
-someDiv.innerHTML = `
-<a href="google.com">Click Me</a>
-asdlfkj
-lkjasdflkas
-${myAge}
-aksdfklj
-abcd
-`;
-*/
-
-//DOMÁCÍ ÚKOL Č. 1
-// const mainContent = document.querySelector('.main-content');
-
-// for (let i = 1; i <= 31; i++) {
-//     const daysDiv = document.createElement('div');
-//     daysDiv.className = 'main-content__day';
-//     mainContent.appendChild(daysDiv);
-//     daysDiv.innerText = [i];    
-// }
-
-
-// INFO ZE 2. LEKCE
 const header = document.querySelector('header.header-news > div.header-news__container'); //může být pouze header-news__container bez šipky a header-news
 
 const carouselItemCount = 2; //upravuje počet zpráv v aplikaci
 let carouselItemStart = 0;
 let articles; //vytáhli jsme proměnnou, kterou používáme ve fci, aby byla globální
+const carousel = new Carousel();
 
-//VYŽÁDÁNÍ DAT ZE SERVERU
-/* verze 1
-const dataPromise = fetch('http://localhost:3000/news.json');
-dataPromise
-.then(serverResponse => serverResponse.text())
-.then(responseText => console.log(JSON.parse(responseText)));
-
-dataPromise.then(serverResponse => console.log(serverResponse.text())); //stažení dat se podaří, volání fce text() - převede do textu
-dataPromise.catch(serverError => alert('Chyba stahování dat.')); //stažení dat se nepodaří
-
-verze 2 - není nutné balit fetch do proměnné, možno volat fetch přímo
-fetch('http://localhost:3000/news.json')
-.then(serverResponse => serverResponse.text())
-.then(responseText => console.log(JSON.parse(responseText)));*/
-
-//verze 3
 fetch('http://localhost:3000/news.json')
     .then(serverResponse => serverResponse.text())
     .then(responseText => {
         const data = JSON.parse(responseText);
         articles = data.articles;
-        populateNewsCarousel(data.articles, carouselItemStart); //data je objekt a přistupuji k articles (které jsou na serveru)
+        carousel.populateNewsCarousel(data.articles, carouselItemStart); //data je objekt a přistupuji k articles (které jsou na serveru)
     });
 
-function populateNewsCarousel(news, startAt) { 
-    header.innerText = ''; //pokud bychom nedali, zprávy při posouvání by se dvojily, tímto se vyčistí div a znovu se tam vytáhnou další zprávy a tak pořád dokola
-    for(let i = startAt; i < (startAt + carouselItemCount); i ++) { //startAt přidáno pro posouvání carouselu
-        const newsValue = news[i];
-        const newsArticle = new NewsArticle();
-        const newsDiv = newsArticle.createDivForNews(newsValue);
-        header.appendChild(newsDiv); //append = připojit na konec, tzn. nahoře se header vyčistí (prázdný string) a zase se přidá další zpráva
-    }
-    checkButtonsVisibilty();
-}
 
-const buttonLeft = document.querySelector('#carousel-button-left');
-const buttonRight = document.querySelector('#carousel-button-right');
-// const buttonLeft = document.getElementById('carousel-button-left');
-// const buttonRight = document.getElementById('carousel-button-right');
 
-buttonLeft.addEventListener('click', () => {
-    carouselItemStart --;
-    populateNewsCarousel(articles, carouselItemStart);
-});
-//verze 2
-// buttonLeft.addEventListener('click', rotateCarouselLeft); //pokud bych nechala rotateCarouselLeft() s uvozovkami, volám fci a při každém reload aplikaci vyskočí okno
 
-buttonRight.addEventListener('click', () => {
-    console.log(carouselItemStart, articles);
-    carouselItemStart ++;
-    populateNewsCarousel(articles, carouselItemStart);
-});
 
-//ÚKOL Č. 2
-function checkButtonsVisibilty(){
-    buttonLeft.hidden = carouselItemStart === 0;
-    buttonRight.hidden = carouselItemStart >= (articles.length - carouselItemCount);
-}
+
+
+
 
