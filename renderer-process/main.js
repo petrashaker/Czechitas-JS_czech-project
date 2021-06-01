fad932264c261b11e1efa3446a68998e3b0033d8
Petra Shaker <petra.shaker@gmail.com>
2021-06-01 10:20:11 +0200

    Lekce 8

--- renderer-process/main.js
+++ renderer-process/main.js
@@ -18,23 +18,42 @@ fetch('http://localhost:3000/news.json')
 const mainContent = document.querySelector('section.main-content');
 
 const currentDate = new Date();
-const maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 2, 0).getDate();
+const maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
 
 for (let i = 1; i <= maxDate; i++) {
     const dayDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
     mainContent.appendChild(new Day(dayDate));
 }
 
-const buttonOpenModal = document.getElementById('open-modal');
-const modalContainer = document.querySelector('.modal-container');
-
-buttonOpenModal.addEventListener('click', () => {
-    modalContainer.hidden = false;
+const openModalButton = document.querySelector('#open-modal');
+openModalButton.addEventListener('click', () => {
+    showDayModal().then((result) => console.log(result));
 });
+ 
+function showDayModal() {
+    const promiseResult = new Promise((resolve, reject) => {
+        const template = document.querySelector('#modal-template');
+        const modal = template.content.cloneNode(true);
 
-// for (let i = 1; i < 31; i++) {
-//     mainContent.appendChild(new Day(i));
-// }
+        const closeAction = () => {
+            const child = document.querySelector('section.modal-container');
+            document.body.removeChild(child);
+            resolve(null);
+        }
 
+        modal.querySelector('#close-modal').addEventListener('click',  closeAction);
+        modal.querySelector('#cancel-button').addEventListener('click', closeAction);
+        modal.querySelector('#save-button').addEventListener('click', () => {
+            const formRef = document.querySelector('#modal-form');
+            const formData = new FormData(formRef);
+            const isHoliday = formData.get('isHolidayControl') === 'on';
+            // resolve('ahoj!!!');
+            resolve({isHoliday : isHoliday});
+        });
 
+        document.body.appendChild(modal);
+    });
+
+    return promiseResult;
+}
 
